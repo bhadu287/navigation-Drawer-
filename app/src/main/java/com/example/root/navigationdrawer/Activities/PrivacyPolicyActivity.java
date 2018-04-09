6d52205da334248a1655954363ae32343efc58f4
root <root@lap0002583.yatraonline.local>
2018-04-09 16:53:10 +0530

    first commit to this project

---
+++ app/src/main/java/com/example/root/navigationdrawer/Activities/PrivacyPolicyActivity.java
@@ -0,0 +1,32 @@
+package com.example.root.navigationdrawer.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.root.navigationdrawer.R;
+
+public class PrivacyPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.notifications, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id =item.getItemId();
+        if(id == R.id.action_clear_notifications){
+            Toast.makeText(getApplicationContext(),"all notification cleared !",Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+}
